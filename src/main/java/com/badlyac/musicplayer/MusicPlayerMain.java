a4f5a227f6f5b7c82ed7ed6f942aa77b736ccc7b
PYMHS <gary9700509@gmail.com>
2025-06-02 10:20:02 +0800

    w

--- src/main/java/com/badlyac/musicplayer/MusicPlayerMain.java
+++ src/main/java/com/badlyac/musicplayer/MusicPlayerMain.java
@@ -8,7 +8,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class MusicPlayerMain extends JavaPlugin {
 
     public static byte[] resourcePackSha1;
-    public static String resourcePackUrl = "https://raw.githubusercontent.com/BadlyacX/MusicPlayerForMinecraftServer/master/src/main/resources/music.zip\n";
+    public static String resourcePackUrl = "https://raw.githubusercontent.com/BadlyacX/MusicPlayerForMinecraftServer/master/src/main/resources/music.zip";
 
     @Override
     public void onEnable() {
